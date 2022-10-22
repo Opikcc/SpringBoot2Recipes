f940efda10ea21c0647c3726c367d610f0b7ca43
Opick <MTaufik@Outlook.Com>
2022-10-23 00:54:27 +0700

    Using Thymeleaf With Spring Boot

--- SpringMVC/src/main/java/com/deapika/SpringMVC/hello/HelloWorldController.java
+++ SpringMVC/src/main/java/com/deapika/SpringMVC/hello/HelloWorldController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
   
-  @GetMapping("/")
-  public String hello() {
-    return "Hello World, From Spring Boot 2 MVC!!!";
-  }
+//  @GetMapping("/")
+//  public String hello() {
+//    return "Hello World, From Spring Boot 2 MVC!!!";
+//  }
 }
---
+++ SpringMVC/src/main/java/com/deapika/SpringMVC/library/web/BookController.java
@@ -0,0 +1,32 @@
+package com.deapika.SpringMVC.library.web;
+
+import com.deapika.SpringMVC.library.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller("bookWeb")
+public class BookController {
+  
+  private final BookService bookService;
+  
+  public BookController(BookService bookService) {
+    this.bookService = bookService;
+  }
+  
+  @GetMapping("/books.html")
+  public String all(Model model) {
+    model.addAttribute("books", bookService.findAll());
+    return "books/list";
+  }
+  
+  @GetMapping(value = "/books.html", params = "isbn")
+  public String get(@RequestParam("isbn") String isbn, Model model) {
+    
+    bookService.find(isbn)
+            .ifPresent(book -> model.addAttribute("book", book));
+    
+    return "books/details";
+  }
+}
